94705321688227df45b0ff240a598be93fc9a1fd
valls <olavalls@gmail.com>
2020-05-27 23:52:02 -0300

    First test with jest and configuring jest

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,29 @@
+import { uuid } from 'uuidv4';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import Appointment from '../../infra/typeorm/entities/Appointment';
+
+class FakeAppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const appointment = this.appointments.find(item => item.date === date);
+    return appointment;
+  }
+
+  public async find(): Promise<Appointment[] | undefined> {
+    return this.appointments ?? [];
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+    this.appointments.push(appointment);
+    return appointment;
+  }
+}
+
+export default FakeAppointmentsRepository;
--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('sum two numbers', () => {
-  expect(1 + 1).toBe(2);
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentRepository = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointmentRepository.execute({
+      date: new Date(),
+      provider_id: '3456321',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('3456321');
+  });
+
+  it('should not be able to create appointment in the same hour', () => {
+    expect(1 + 1).toBe(2);
+  });
 });
